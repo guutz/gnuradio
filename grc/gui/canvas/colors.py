5b1fff2f84a0674ad49fa2e56e2141e62220cf7d
Michael Gutierrez <findmagpie@gmail.com>
2022-07-28 13:51:26 -0400

    Update colors.py

--- grc/gui/canvas/colors.py
+++ grc/gui/canvas/colors.py
@@ -26,10 +26,10 @@ def get_color(color_code):
 #################################################################################
 
 
-HIGHLIGHT_COLOR = get_color('#00FFFF')
-BORDER_COLOR = get_color('#616161')
-BORDER_COLOR_DISABLED = get_color('#888888')
-FONT_COLOR = get_color('#000000')
+HIGHLIGHT_COLOR = get_color('#FFFF00')
+BORDER_COLOR = get_color('#AAAAAA')
+BORDER_COLOR_DISABLED = get_color('#777777')
+FONT_COLOR = get_color('#CCCCCC')
 
 # Missing blocks stuff
 MISSING_BLOCK_BACKGROUND_COLOR = get_color('#FFF2F2')
@@ -42,21 +42,21 @@ BLOCK_DEPRECATED_BACKGROUND_COLOR = get_color('#FED86B')
 BLOCK_DEPRECATED_BORDER_COLOR = get_color('#FF540B')
 
 # Flow graph color constants
-FLOWGRAPH_BACKGROUND_COLOR = get_color('#FFFFFF')
-COMMENT_BACKGROUND_COLOR = get_color('#F3F3F3')
+FLOWGRAPH_BACKGROUND_COLOR = get_color('#222222')
+COMMENT_BACKGROUND_COLOR = get_color('#DDDDDD')
 FLOWGRAPH_EDGE_COLOR = COMMENT_BACKGROUND_COLOR
 
 # Block color constants
-BLOCK_ENABLED_COLOR = get_color('#F1ECFF')
-BLOCK_DISABLED_COLOR = get_color('#CCCCCC')
-BLOCK_BYPASSED_COLOR = get_color('#F4FF81')
+BLOCK_ENABLED_COLOR = get_color('#340000')
+BLOCK_DISABLED_COLOR = get_color('#888888')
+BLOCK_BYPASSED_COLOR = get_color('#A4AF31')
 
 # Connection color constants
-CONNECTION_ENABLED_COLOR = get_color('#000000')
-CONNECTION_DISABLED_COLOR = get_color('#BBBBBB')
+CONNECTION_ENABLED_COLOR = get_color('#FFFFFF')
+CONNECTION_DISABLED_COLOR = get_color('#343434')
 CONNECTION_ERROR_COLOR = get_color('#FF0000')
 
-DEFAULT_DOMAIN_COLOR = get_color('#777777')
+DEFAULT_DOMAIN_COLOR = get_color('#333333')
 
 
 #################################################################################
@@ -86,29 +86,26 @@ DARK_THEME_STYLES = b"""
                          #dtype_bool            { background-color: #00FF99; }
                          #dtype_hex             { background-color: #00FF99; }
                          #dtype_string          { background-color: #CC66CC; }
-                         #dtype_id              { background-color: #DDDDDD; }
-                         #dtype_stream_id       { background-color: #DDDDDD; }
-                         #dtype_raw             { background-color: #DDDDDD; }
+                         #dtype_id              { background-color: #222222; }
+                         #dtype_stream_id       { background-color: #222222; }
+                         #dtype_raw             { background-color: #222222; }
 
-                         #enum_custom           { background-color: #EEEEEE; }
+                         #enum_custom           { background-color: #222222; }
                      """
 LIGHT_THEME_STYLES = b"""
                         #dtype_complex         { background-color: #3399FF; }
                         #dtype_real            { background-color: #FF8C69; }
                         #dtype_float           { background-color: #FF8C69; }
                         #dtype_int             { background-color: #00FF99; }
-
                         #dtype_complex_vector  { background-color: #3399AA; }
                         #dtype_real_vector     { background-color: #CC8C69; }
                         #dtype_float_vector    { background-color: #CC8C69; }
                         #dtype_int_vector      { background-color: #00CC99; }
-
                         #dtype_bool            { background-color: #00FF99; }
                         #dtype_hex             { background-color: #00FF99; }
                         #dtype_string          { background-color: #CC66CC; }
-                        #dtype_id              { background-color: #DDDDDD; }
-                        #dtype_stream_id       { background-color: #DDDDDD; }
-                        #dtype_raw             { background-color: #FFFFFF; }
-
-                        #enum_custom           { background-color: #EEEEEE; }
+                        #dtype_id              { background-color: #333333; }
+                        #dtype_stream_id       { background-color: #333333; }
+                        #dtype_raw             { background-color: #333333; }
+                        #enum_custom           { background-color: #333333; }
                     """
